633f460f14655e1d624076888825dd55fe1b82f6
Aliakbargh85 <131409388+Aliakbargh85@users.noreply.github.com>
2025-10-29 21:52:35 +0330

    Add files via upload

---
+++ Todo List/js/script.js
@@ -0,0 +1,44 @@
+﻿let $ = document;
+
+let inputElem = $.querySelector("input");
+let addTodoForm = $.querySelector(".add");
+let todoUlElem = $.querySelector(".todos");
+
+function addNewTodoItem(todoText) {
+  let newTodoLi = $.createElement("li");
+  newTodoLi.className =
+    "list-group-item d-flex justify-content-between align-items-center";
+
+  let newTodoTitleSpan = $.createElement("span");
+  newTodoTitleSpan.innerHTML = todoText;
+
+  let newTodoTrash = $.createElement("i");
+  newTodoTrash.className = "fa fa-trash-o delete";
+
+  newTodoTrash.addEventListener("click", function (e) {
+    e.target.parentElement.remove();
+  });
+
+  newTodoLi.append(newTodoTitleSpan, newTodoTrash);
+
+  todoUlElem.append(newTodoLi);
+
+  console.log(newTodoLi);
+}
+
+addTodoForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+inputElem.addEventListener("keydown", function (e) {
+  let inputValue = e.target.value.trim();
+
+  if (e.key === "Enter") {
+    if (inputValue !== "") {
+      inputElem.value = "";
+      addNewTodoItem(inputValue);
+    } else {
+      alert("کادر را پر کنید");
+    }
+  }
+});
